77fda6ecd974f22e65890bfa4c6fa4f768608804
Julian Hahn <40185075+julianhahn@users.noreply.github.com>
2024-07-05 10:40:50 +0900

    add analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+import withBundleAnalyzer from '@next/bundle-analyzer'
+const bundleAnalyzer = withBundleAnalyzer({
+    enabled: process.env.ANALYZE === 'true',
 })
- 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
- 
-module.exports = withBundleAnalyzer(nextConfig)
+
+export default bundleAnalyzer(nextConfig)
